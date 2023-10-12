04f8a2519240ad4924ffdb6e2fa401997a3ac4f6
The_Creator <ganeshm995@hotmail.com>
2023-10-12 23:53:41 +0530

    Added JSON extension method to support object and dictionary conversion

---
+++ Creator.Infinity.Json/Configuration.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Creator.Infinity.Json
+{
+    using Newtonsoft.Json;
+
+    internal class Configuration
+    {
+        public static JsonSerializerSettings JsonConvertDefaultSettings()
+        {
+            return new JsonSerializerSettings
+            {
+                PreserveReferencesHandling = PreserveReferencesHandling.All,
+                ReferenceLoopHandling = ReferenceLoopHandling.Serialize
+            };
+        }
+    }
+}
---
+++ Creator.Infinity.Json/Extensions.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Creator.Infinity.Json
+{
+    using Newtonsoft.Json;
+    using System.Collections.Generic;
+
+    public static class Extensions
+    {
+        public static string ToJson<TKey, TValue>(this Dictionary<TKey, TValue> dictionary)
+        {
+            return dictionary == null ? "" : JsonConvert.SerializeObject(dictionary, Configuration.JsonConvertDefaultSettings());
+        }
+
+        public static string ToJson(this object obj)
+        {
+            return obj == null ? "" : JsonConvert.SerializeObject(obj, Configuration.JsonConvertDefaultSettings());
+        }
+    }
+}
